56f9fbf8677a13d2e8a6b8d43afa6e0d82dec714
abhishekkushwaha29 <168764393+abhishekkushwaha29@users.noreply.github.com>
2024-05-02 22:27:33 +0530

    Delete Web Based Movie Rating Prediction System directory

--- Web Based Movie Rating Prediction System/MlApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    
-    path('', views.predictor, name="predictor"),
-    path('result', views.formInfo, name="result")
-    
-]
